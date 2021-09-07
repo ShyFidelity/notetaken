15e84f09b75b1cbfc95021ba421b80b2ebe19221
jkworks <info@jilliankayworks.com>
2021-09-07 11:17:31 -0600

    routed to index.html

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const path = require('path');
-//change
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js.js');
+//changenp
 const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-//test test test 
-// Import custom middleware, "cLog"
-app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+
 
 app.use(express.static('public'));
 
@@ -25,10 +20,6 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
 
 app.get('*', (req, res) =>
 res.sendFile(path.join(__dirname, '/public/pages/404.html')));

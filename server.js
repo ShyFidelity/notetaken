9a719fb92b89a63ead5201e5ff33d8eb05d23dfc
jkworks <info@jilliankayworks.com>
2021-09-07 22:10:18 -0600

    added app.post

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const routes = require('./routes/routes');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -19,10 +18,21 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.post('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.post('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 
 app.get('*', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/404.html')));
+res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)

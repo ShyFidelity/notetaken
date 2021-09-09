c4ade1148972f0fadc4b1ecee126f37559cdc60f
jkworks <info@jilliankayworks.com>
2021-09-08 21:44:00 -0600

    adding routes page

---
+++ routes/notes.js
@@ -0,0 +1,41 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new note
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, note } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      note,
+      note_id: uuid(),
+    };
+
+
+//   const { username, topic, tip } = req.body;
+
+//   if (req.body) {
+//     const newTip = {
+//       username,
+//       tip,
+//       topic,
+//       tip_id: uuid(),
+//     };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added you organized person`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+module.exports = notes;
